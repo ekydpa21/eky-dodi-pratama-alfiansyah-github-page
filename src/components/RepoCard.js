69d7dc6a94dde876217d2897bc24d2bd27c77d40
ekydpa21 <ekydodipratamaa21@gmail.com>
2021-05-31 18:29:18 +0700

    fetch api sudah berfungsi, development page repositories on progress

---
+++ src/components/RepoCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function RepoCard({ repo }) {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-body">This is some text within a card body.</div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Repositories.js
+++ src/pages/Repositories.js
@@ -1,21 +1,26 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import RepoCard from "../components/RepoCard"
 
 export default function Repositories() {
   const { username } = useParams()
-  const [fetchedData, setFetchedData] = useState()
+  const [fetchedRepos, setFetchedRepos] = useState()
 
   useEffect(() => {
     axios.get(`https://api.github.com/users/${username}/repos`).then(({ data }) => {
-      setFetchedData(data)
+      setFetchedRepos(data)
     })
   }, [username])
 
-  console.log(fetchedData)
   return (
-    <div className="Page">
-      <h1>Repositories</h1>
+    <div className="Repositories">
+      <h1>
+        {fetchedRepos &&
+          fetchedRepos.map((repo, idx) => {
+            return <RepoCard key={idx} repo={repo} />
+          })}
+      </h1>
     </div>
   )
 }
