458d1e7c55f206252bf044361fed02b64b8a61a7
ekydpa21 <ekydodipratamaa21@gmail.com>
2021-06-01 13:54:18 +0700

    development page repositories selesai

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -1,14 +1,19 @@
+import moment from "moment"
 import React from "react"
 import { Link } from "react-router-dom"
 
 export default function RepoCard({ repo }) {
-  const { name, language, html_url } = repo
-  console.log(repo)
+  const { name, language, html_url, updated_at } = repo
+  const updatedTime = moment(updated_at).format("YYYYMMDD")
+
   return (
     <Link className="link" to={{ pathname: html_url }} target="_blank">
       <div className="repo-info">
         <h4>{name}</h4>
-        <p>{language}</p>
+        <div className="project-info">
+          {language && <p className="me-4">{language}</p>}
+          <p>Updated {moment(updatedTime, "YYYYMMDD").fromNow()}</p>
+        </div>
       </div>
     </Link>
   )
--- src/pages/Repositories.js
+++ src/pages/Repositories.js
@@ -1,43 +1,43 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router";
-import Swal from "sweetalert2";
-import LoadingScreen from "../components/LoadingScreen";
-import RepoCard from "../components/RepoCard";
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router"
+import Swal from "sweetalert2"
+import LoadingScreen from "../components/LoadingScreen"
+import RepoCard from "../components/RepoCard"
 
 export default function Repositories() {
-  const history = useHistory();
-  const { username } = useParams();
-  const [fetchedUser, setFetchedUser] = useState();
-  const [fetchedRepos, setFetchedRepos] = useState();
+  const history = useHistory()
+  const { username } = useParams()
+  const [fetchedUser, setFetchedUser] = useState()
+  const [fetchedRepos, setFetchedRepos] = useState()
 
   useEffect(() => {
     axios
       .get(`https://api.github.com/users/${username}`)
       .then(({ data }) => {
-        console.log(data);
-        setFetchedUser(data);
+        console.log(data)
+        setFetchedUser(data)
       })
       .catch(({ response }) => {
         Swal.fire({
           icon: "error",
           title: "Oops...",
           text: `User ${response.data.message}`,
-        });
-        history.push("/");
-      });
-  }, [username, history]);
+        })
+        history.push("/")
+      })
+  }, [username, history])
 
   useEffect(() => {
     axios
       .get(`https://api.github.com/users/${username}/repos`)
       .then(({ data }) => {
-        setFetchedRepos(data);
+        setFetchedRepos(data)
       })
-      .catch(({ response }) => console.log(response.data.message));
-  }, [username]);
+      .catch(({ response }) => console.log(response.data.message))
+  }, [username])
 
-  if (!fetchedUser || !fetchedRepos) return <LoadingScreen />;
+  if (!fetchedUser || !fetchedRepos) return <LoadingScreen />
 
   return (
     <div className="Repositories">
@@ -47,15 +47,18 @@ export default function Repositories() {
             <img src={fetchedUser.avatar_url} alt="user-avatar" />
             <h3>{fetchedUser.name}</h3>
             <h5>{fetchedUser.login}</h5>
+            <p>
+              {fetchedUser.followers} Follower . {fetchedUser.following} Following
+            </p>
           </>
         )}
       </div>
       <div className="repo-list">
         {fetchedRepos &&
           fetchedRepos.map((repo, idx) => {
-            return <RepoCard key={idx} repo={repo} />;
+            return <RepoCard key={idx} repo={repo} />
           })}
       </div>
     </div>
-  );
+  )
 }
